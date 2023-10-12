ed81deed573c07c74d22774f0afa390a8e5690bb
Rati <rziraqashvili@gmail.com>
2023-10-12 13:32:25 +0400

    created search server and members functionality

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,18 +2,49 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "@/node_modules/next/navigation";
 //@ts-ignore
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
+import { ScrollArea } from "../ui/scroll-area";
 import { ServerHeader } from "./server-header";
+import { ServerSearch } from "./server-search";
+//@ts-ignore
+import { Hash, Mic, Video, ShieldCheck, ShieldAlert } from "lucide-react";
 
 interface ServerSidebarProps {
   serverId: string;
 }
 
+interface Channel {
+  id: string;
+  name: string;
+  type: ChannelType;
+}
+
+interface Member {
+  profileId: string;
+  id: string;
+  profile: {
+    name: string;
+  };
+  role: MemberRole;
+}
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
+};
+
 export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const profile = await currentProfile();
 
   if (!profile) {
-    redirect("/");
+    return redirect("/");
   }
 
   const server = await db.server.findUnique({
@@ -38,29 +69,75 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   });
 
   const textChannels = server?.channels.filter(
-    (channel: { type: any }) => channel.type === ChannelType.TEXT
+    (channel: Channel) => channel.type === ChannelType.TEXT
   );
   const audioChannels = server?.channels.filter(
-    (channel: { type: any }) => channel.type === ChannelType.AUDIO
+    (channel: Channel) => channel.type === ChannelType.AUDIO
   );
   const videoChannels = server?.channels.filter(
-    (channel: { type: any }) => channel.type === ChannelType.VIDEO
+    (channel: Channel) => channel.type === ChannelType.VIDEO
   );
   const members = server?.members.filter(
-    (member: { profileId: string }) => member.profileId !== profile.id
+    (member: Member) => member.profileId !== profile.id
   );
+
 
   if (!server) {
     return redirect("/");
   }
 
   const role = server.members.find(
-    (member: any) => member.profileId === profile.id
+    (member: Member) => member.profileId === profile.id
   )?.role;
+
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d32] bg-[#f2f3f5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel: Channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel: Channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels?.map((channel: Channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members?.map((member: Member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
