2e88580619ecc19240cd7703ebd9cd0b2eb329e7
Rati <rziraqashvili@gmail.com>
2023-10-12 11:09:54 +0400

    finished deleting server functionallity

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,31 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { NextResponse } from "@/node_modules/next/server"
 
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const profile = await currentProfile();
+
+        if(!profile){
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+        })
+
+        return NextResponse.json(server)
+    } catch (error) {
+        console.log("[server_id_delete]", error)
+        return new NextResponse("internal error", { status: 500 })
+    }
+}
+
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string } }
@@ -31,3 +56,4 @@ export async function PATCH(
         return new NextResponse("internal error", { status: 500 })
     }
 }
+
