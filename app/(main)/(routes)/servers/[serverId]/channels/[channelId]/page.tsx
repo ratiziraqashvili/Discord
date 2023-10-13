a466e7cfabee44228cb596c6caaeecea378fd5c4
Rati <rziraqashvili@gmail.com>
2023-10-13 15:28:59 +0400

    Created Chat Header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,45 @@
-const ChannelIdPage = () => {
-    return ( 
-        <div>
-            channel id page
-        </div>
-     );
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "@/node_modules/next/navigation";
+//@ts-ignore
+import { redirectToSignIn } from "@clerk/nextjs";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
- 
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+        id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+        serverId: params.serverId,
+        //@ts-ignore
+        profileId: profile.id,
+    }
+  });
+
+  if(!channel || !member){
+    redirect("/")
+  }
+
+  return <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+    <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+  </div>;
+};
+
 export default ChannelIdPage;
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,28 @@
+//@ts-ignore
+import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "./ui/button";
+import NavigationSidebar from "./navigation/navigation-sidebar";
+import { ServerSidebar } from "./server/server-sidebar";
+
+export const MobileToggle = ({ serverId }: { serverId: string }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        {/*@ts-ignore*/}
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          {/*@ts-ignore*/}
+          <NavigationSidebar />
+        </div>
+        {/*@ts-ignore*/}
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
