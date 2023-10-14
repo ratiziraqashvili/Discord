8ef8da25ce030cc698aecf3552ec614b8d08eb8b
Rati <rziraqashvili@gmail.com>
2023-10-14 16:04:22 +0400

    Conversations set up

--- lib/conversation.ts
+++ lib/conversation.ts
@@ -2,6 +2,12 @@ import { db } from "./db";
 
 export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
     let conversation = await findConversation(memberOneId, memberTwoId) || await findConversation(memberTwoId, memberOneId)
+
+    if(!conversation){
+        conversation = await createNewConversation(memberOneId, memberTwoId)
+    } 
+
+    return conversation ;
 }
 
 const findConversation = async (memberOneId: string, memberTwoId: string) => {
