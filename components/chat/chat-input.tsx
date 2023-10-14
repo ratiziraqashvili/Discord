bcf161b060c1622433a0ff03ff2f58ad0956a657
Rati <rziraqashvili@gmail.com>
2023-10-14 17:25:20 +0400

    Created input component

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -3,7 +3,17 @@
 import * as z from "zod";
 //@ts-ignore
 import { useForm } from "react-hook-form";
+//@ts-ignore
 import { zodResolver } from "@hookform/resolvers/zod";
+//@ts-ignore
+import axios from "axios"
+//@ts-ignore
+import qs from "query-string"
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+//@ts-ignore
+import { Plus, Smile } from "lucide-react";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -24,7 +34,55 @@ export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
     },
   });
 
-  const isLoading = form.formState.isSubmitting
+  const isLoading = form.formState.isSubmitting;
 
-  return <div>chat input</div>;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+        const url = qs.stringifyUrl({
+            url: apiUrl,
+            query,
+        })
+
+        await axios.post(url, values);
+    } catch (error) {
+        console.log(error)
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }: any) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 darl:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                  >
+                    <Plus className="text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
+                    {...field}
+                    disabled={isLoading}
+                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                  />
+                  <div className="absolute top-7 right-8">
+                    <Smile />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 };
