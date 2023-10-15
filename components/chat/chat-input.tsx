154c56bbf406b6f04e9780d2012ed52e38aee211
Rati <rziraqashvili@gmail.com>
2023-10-15 17:34:09 +0400

    Added emoji bar

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -6,17 +6,20 @@ import { useForm } from "react-hook-form";
 //@ts-ignore
 import { zodResolver } from "@hookform/resolvers/zod";
 //@ts-ignore
-import axios from "axios"
+import axios from "axios";
 //@ts-ignore
-import qs from "query-string"
+import qs from "query-string";
+
+import React from "react";
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 //@ts-ignore
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 //@ts-ignore
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "../emoji-picker";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -32,7 +35,7 @@ const formSchema = z.object({
 export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
   const { onOpen } = useModal();
 
-  const router = useRouter()
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,15 +48,17 @@ export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-        const url = qs.stringifyUrl({
-            url: apiUrl,
-            query,
-        })    
-        
-        await axios.post(url, values);
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
 
+      await axios.post(url, values);
+
+      form.reset();
+      router.refresh();
     } catch (error) {
-        console.log("this is that error", error)
+      console.log("this is that error", error);
     }
   };
 
@@ -83,7 +88,11 @@ export const ChatInput = ({ apiUrl, query, type, name }: ChatInputProps) => {
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
