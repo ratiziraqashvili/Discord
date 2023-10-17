d8fec675a2b5f0e58a6ca88df802caaf77a35b38
Rati <rziraqashvili@gmail.com>
2023-10-17 16:14:02 +0400

    Added design to render pdf and images

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -4,7 +4,10 @@ import { Member, Profile, MemberRole } from "@prisma/client";
 import { ActionTooltip } from "../action-tooltip";
 import { UserAvatar } from "../user-avatar";
 //@ts-ignore
-import { ShieldCheck, ShieldAlert } from "lucide-react"
+import { ShieldCheck, ShieldAlert, FileIcon } from "lucide-react";
+import Image from "@/node_modules/next/image";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface ChatItemProps {
   id: string;
@@ -22,10 +25,10 @@ interface ChatItemProps {
 }
 
 const roleIconMap: MemberRole = {
-    "GUEST" : null,
-    "MODERATOR" : <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
-    "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
-}
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 export const ChatItem = ({
   id,
@@ -39,6 +42,19 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const fileType = fileUrl?.split(".").pop();
+
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.id === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
+
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
@@ -56,12 +72,52 @@ export const ChatItem = ({
               </ActionTooltip>
             </div>
             <span className="text-xs text-zinc-500 dark:text-zinc-400">
-                {timestamp}
+              {timestamp}
             </span>
           </div>
-            {content}
+          {isImage && (
+            <a
+              href={fileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                className="object-cover"
+              />
+            </a>
+          )}
+          {isPDF && (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+              <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+              <a
+                href={fileUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+              >
+                PDF file
+              </a>
+            </div>
+          )}
+          {!fileUrl && !!isEditing && (
+            <p className={cn("text-sm text-zinc-600 dark:text-zinc-300", deleted && "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1")}>
+                {content}
+                {isUpdated && !deleted && (
+                    <span className="text-[10px] mx-2 text-zinc-500 dark:text-zinc-400">(edited)</span>
+                )}
+            </p>
+          )}
         </div>
       </div>
+      {canDeleteMessage && (
+        <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+
+        </div>
+      )}
     </div>
   );
 };
