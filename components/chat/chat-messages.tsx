5cda2ae83edeffee0518fe0f08764e4b03b33984
Rati <rziraqashvili@gmail.com>
2023-10-18 17:16:55 +0400

    Added chat scroll hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,7 +5,7 @@ import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 //@ts-ignore
 import { Loader2, ServerCrash } from "lucide-react";
-import { Fragment } from "react";
+import { Fragment, useRef, ElementRef } from "react";
 import { ChatItem } from "./chat-item";
 //@ts-ignore
 import { format } from "date-fns";
@@ -46,6 +46,9 @@ export const ChatMessages = ({
   const addKey = `chat:${chatId}:messages`
   const updateKey = `chat:${chatId}:messages:update`
 
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
+
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
       queryKey,
@@ -78,7 +81,7 @@ export const ChatMessages = ({
   }
 
   return (
-    <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+    <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
       <ChatWelcome type={type} name={name} />
       <div className="flex flex-col-reverse mt-auto">
@@ -102,6 +105,7 @@ export const ChatMessages = ({
           </Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 };
--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -9,45 +9,48 @@ interface ChatQueryProps {
     queryKey: string;
     apiUrl: string;
     paramKey: "channelId" | "conversationId";
-    paramValue: string
-}
-
-export const useChatQuery = ({
-    queryKey, apiUrl, paramKey, paramValue
-}: ChatQueryProps) => {
+    paramValue: string;
+  };
+  
+  export const useChatQuery = ({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue
+  }: ChatQueryProps) => {
     const { isConnected } = useSocket();
-
+  
     const fetchMessages = async ({ pageParam = undefined }) => {
-        const url = qs.stringifyUrl({
-            url: apiUrl,
-            query: {
-                cursor: pageParam,
-                [paramKey]: paramValue
-            }
-        }, { skipNull: true });
-
-        const res = await fetch(url)
-        return res.json();
-    }
-
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        }
+      }, { skipNull: true });
+  
+      const res = await fetch(url);
+      return res.json();
+    };
+  
     const {
-        data,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-        status,
+      data,
+      fetchNextPage,
+      hasNextPage,
+      isFetchingNextPage,
+      status,
     } = useInfiniteQuery({
-        queryKey: [queryKey],
-        queryFn: fetchMessages,
-        getNextPageParam: (lastPage: any) => lastPage?.nextCursor,
-        refetchInterval: isConnected ? false : 1000
+      queryKey: [queryKey],
+      queryFn: fetchMessages,
+      getNextPageParam: (lastPage: any) => lastPage?.nextCursor,
+      refetchInterval: isConnected ? false : 1000,
     });
-
+  
     return {
-        data,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-        status
-    }
-};
+      data,
+      fetchNextPage,
+      hasNextPage,
+      isFetchingNextPage,
+      status,
+    };
+  }
