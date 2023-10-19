0640bcc9e6786b3122f7a70593d431024be94c53
Rati <rziraqashvili@gmail.com>
2023-10-19 18:46:07 +0400

    fixed some issues

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -41,7 +41,7 @@ interface ChatItemProps {
   socketQuery: Record<string, string>;
 }
 
-const roleIconMap: MemberRole = {
+const roleIconMap: any = {
   GUEST: null,
   MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
   ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
